9af75a71fd08d40ed3f1a78c3d3d4c2fad857e37
CybLiz <133205866+CybLiz@users.noreply.github.com>
2024-02-10 18:02:29 +0100

    background image added! started html structure ...

--- Front/src/Pages/MysteryNumberMain.jsx
+++ Front/src/Pages/MysteryNumberMain.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import "./MysteryNumberMain.css";
  
 
 
 const MysteryNumberMain = () => {
 	return (
-    <>
-		<Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
+    
+<div className="mysterNumberGame">
+  <div className='title'>
+    <h1>Mystery Number</h1>
+  </div>
+  <div className='numberSection'>
+  <button className='level'>Level 1</button>
+  <button className='level'>Level 2</button>
+  <button className='level' >Level 3</button>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    </>
+  </div>
+  <div className='restartSection'>
+  <button>Restart</button>
+
+  </div>
+  
+  <div className='resultSection'></div>
+
+    </div>
+
+    
 	)
 }
 
