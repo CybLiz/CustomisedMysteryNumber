b779ad0d45dfc12579e75e4934deddb4e97d5de5
CybLiz <133205866+CybLiz@users.noreply.github.com>
2024-02-13 16:17:54 +0100

    i added a generateMysteryNumber function that generate an accurate  MN for each level, added two png for victory or failure that appear after the last  result message, ajusted its css ! issues: two MN are generated at once! the + & - indication still not functionning correctly ! button restart is added but still not functionnal

--- Front/src/Pages/MysteryNumberMain.jsx
+++ Front/src/Pages/MysteryNumberMain.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import "./MysteryNumberMain.css";
 import failedImage from '../failed.png';
+import succesImage from '../Succes.png';
 
 // import { GiFeather } from "react-icons/gi";
 {/* <GiFeather /> */ }
-
-
-
-
 
 
 // const handleResult = (number) => {
@@ -39,74 +36,76 @@ const MysteryNumberMain = () => {
   const [chance, setChance] = useState(1);
   const [result, setResult] = useState([]);
   const [gameOver, setGameOver] = useState(false);
+  const [level, setLevel] = useState(1);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
 
 
   // const mysteryNumber = Math.floor(Math.random() * 100) + 1;
   // console.log(mysteryNumber);
 
 
-  const [level, setLevel] = useState(1);
   const handleLevelClick = (level) => {
-  setLevel(level);
-  setSelectedNumber('');
-}
+    setLevel(level);
+    setSelectedNumber('');
+    const mysteryNumber = generateMysteryNumber(level);
+  }
 
-const handleNumberClick = (number) => {
-  if (!gameOver) {
-    setSelectedNumber(number);
-    handleResult(number);
-    console.log(selectedNumber)
-  }
-};
+  const generateMysteryNumber = (level) => {
+    let start, end;
+    if (level === 1) {
+      start = 1;
+      end = 10;
+    }
+    if (level === 2) {
+      start = 1;
+      end = 50;
+    }
+    if (level === 3) {
+      start = 1;
+      end = 100;
+    }
+    return Math.floor(Math.random() * (end)) + 1;
+  };
 
-const generateMysteryNumber = (level) => {
-  let start, end;
-  if (level === 1) {
-    start= 1;
-    end=10;
-  }
-  if (level === 2) {
-    start= 1;
-    end=50;
-  }
-  if (level === 3) {
-    start= 1;
-    end=100;
-}
-return Math.floor(Math.random() * (end)) + 1;
 
-};
+  const mysteryNumber = generateMysteryNumber(level);
+  const handleNumberClick = (number) => {
+    if (!gameOver) {
+      setSelectedNumber(number);
+      handleResult(number, mysteryNumber);
+      console.log(selectedNumber)
+    }
+  };
 
-const mysteryNumber = generateMysteryNumber(level);
-
-const levelNumbControll = () => {
-  const numbers = [];
-  let start, end;
-  if (level === 1) {
-    start= 1;
-    end=10;
-  }
-  if (level === 2) {
-    start= 1;
-    end=50;
-  }
-  if (level === 3) {
-    start= 1;
-    end=100;
-  }
-  for (let i= start; i <= end; i++){
-    numbers.push(
-      <button key={i} className="numberBtn" onClick={() => handleNumberClick(i)}>
+  const levelNumbControll = () => {
+    const numbers = [];
+    let start, end;
+    if (level === 1) {
+      start = 1;
+      end = 10;
+    }
+    if (level === 2) {
+      start = 1;
+      end = 50;
+    }
+    if (level === 3) {
+      start = 1;
+      end = 100;
+    }
+    for (let i = start; i <= end; i++) {
+      numbers.push(
+        <button key={i} className="numberBtn" onClick={() => handleNumberClick(i)}>
           {i}
-      </button>
-  );
-  }
-  return numbers;
-};
+        </button>
+      );
+    }
+    return numbers;
+  };
+  console.log(mysteryNumber);
 
 
-
-  const handleResult = (number) => {
+  const handleResult = (number, mysteryNumber) => {
     let message = '';
     if (chance <= 3) {
       if (number < mysteryNumber) {
@@ -114,16 +113,26 @@ const levelNumbControll = () => {
       } else if (number > mysteryNumber) {
         message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} ? ... -`;
       } else if (number === mysteryNumber) {
-        message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} Congratulations!You guessed the mystery number`;
+        message =
+
+          <p>
+            {`${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} Congratulations! You guessed the mystery number`}
+          </p>
+
         setGameOver(true)
+        setSuccess(true);
       }
 
       setChance(chance + 1);
 
 
     } else {
-      message = `Sorry, you ran out of chances. The mystery number was ${mysteryNumber}.`;
+      message = 
+
+        <p> Sorry, you ran out of chances. The mystery number was ${mysteryNumber}. </p>
+
       setGameOver(true)
+      setFailure(true);
     }
     setResult(prevResult => prevResult.concat(message));
   };
@@ -140,23 +149,31 @@ const levelNumbControll = () => {
             {levelNumbControll()}
           </div>
           <div className='levels'>
-            <button className= {level === 1 ? 'level active' : 'level'} onClick={() => handleLevelClick(1)}>Level 1</button>
-            <button className= {level === 2 ? 'level active' : 'level'} onClick={() => handleLevelClick(2)}>Level 2</button>
-            <button className= {level === 3 ? 'level active' : 'level'} onClick={() => handleLevelClick(3)}>Level 3</button>
-           
+            <button className={level === 1 ? 'level active' : 'level'} onClick={() => handleLevelClick(1)}>Level 1</button>
+            <button className={level === 2 ? 'level active' : 'level'} onClick={() => handleLevelClick(2)}>Level 2</button>
+            <button className={level === 3 ? 'level active' : 'level'} onClick={() => handleLevelClick(3)}>Level 3</button>
+
           </div>
 
         </div>
 
-        {/* <div className='restartSection'>
-          <button>Restart</button>
-        </div> */}
+        <div className='restartSection'>
+          <button className='restartbtn'>Restart</button>
+        </div>
 
 
-        <div className='right'>
-          {result.map((message, index) => (
-            <p key={index}>{message}</p>
-          ))}      </div>
+<div className='right'>
+
+<div className='gameOver'>
+        {success && <img src={succesImage} alt="Success" />}
+        {failure && <img src={failedImage} alt="Failure" />}
+        </div>
+        <div className='result'>{result.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}</div>
+        
+       
+      </div>
       </div>
     </div>
   );
