8650cb5030ab98dbd8b4c37856de6d9433beb795
CybLiz <133205866+CybLiz@users.noreply.github.com>
2024-02-13 12:23:16 +0100

    added levelNumbControll function to set up the diff levels and the availible numb for each thanks to active class, it works but mystery number dosn't fit each level! have to create a function that generate a mysteryNumb  for each level...

--- Front/src/Pages/MysteryNumberMain.jsx
+++ Front/src/Pages/MysteryNumberMain.jsx
@@ -10,101 +10,137 @@ import failedImage from '../failed.png';
 
 
 
-  // const handleResult = (number) => {
-  //   let message = '';
-  //   let currentAttempt = 4 - chance;
-  //   if (chance > 0) {
-  //     if (number < mysteryNumber) {
-  //       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try : ${number} ? ... +`;
-  //     } else if (number > mysteryNumber) {
-  //       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number} ? ... -`;
-  //     } else if (number === mysteryNumber) {
-  //       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number} <br>Congratulations!`;
-  //       setGameOver(true);
-  //     }
-  //     setChance(chance - 1);
-  //   } else {
-  //     message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number}  <br> The mystery number was ${mysteryNumber}.`;
-  //     setGameOver(true);
-  //   }
-  //   setChance(chance - 1);
+// const handleResult = (number) => {
+//   let message = '';
+//   let currentAttempt = 4 - chance;
+//   if (chance > 0) {
+//     if (number < mysteryNumber) {
+//       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try : ${number} ? ... +`;
+//     } else if (number > mysteryNumber) {
+//       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number} ? ... -`;
+//     } else if (number === mysteryNumber) {
+//       message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number} <br>Congratulations!`;
+//       setGameOver(true);
+//     }
+//     setChance(chance - 1);
+//   } else {
+//     message = `${currentAttempt}${currentAttempt === 1 ? 'st' : currentAttempt === 2 ? 'nd' : currentAttempt === 3 ? 'rd' : 'th'} Try :${number}  <br> The mystery number was ${mysteryNumber}.`;
+//     setGameOver(true);
+//   }
+//   setChance(chance - 1);
 
-  //   setResult(prevResult => prevResult.concat(message));
+//   setResult(prevResult => prevResult.concat(message));
 
 
-  // };
+// };
 
-  const MysteryNumberMain = () => {
-    const [selectedNumber, setSelectedNumber] = useState('');
-    const [chance, setChance] = useState(1);
-    const [result, setResult] = useState([]);
-    const [gameOver, setGameOver] = useState(false);
-   
-  
-    const mysteryNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(mysteryNumber);
-  
-    const handleNumberClick = (number) => {
-      if (!gameOver){
+const MysteryNumberMain = () => {
+  const [selectedNumber, setSelectedNumber] = useState('');
+  const [chance, setChance] = useState(1);
+  const [result, setResult] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+
+  const mysteryNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(mysteryNumber);
+
+  const handleNumberClick = (number) => {
+    if (!gameOver) {
       setSelectedNumber(number);
       handleResult(number);
       console.log(selectedNumber)
-        }    };
+    }
+  };
+
+  const [level, setLevel] = useState(1);
 
 
-    const handleResult = (number) => {
-      let message = '';
-      if (chance <= 3) {
-        if (number < mysteryNumber) {
-          message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} ? ... +`;
-        } else if (number > mysteryNumber) {
-          message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} ? ... -`;
-        } else if (number === mysteryNumber) {
-          message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} Congratulations!`;
-          setGameOver(true)        }
-        setChance(chance + 1);
+const handleLevelClick = (level) => {
+  setLevel(level);
+  setSelectedNumber('');
+}
 
-      } else {
-        message = `Sorry, you ran out of chances. The mystery number was ${mysteryNumber}.`;
-        setGameOver(true)      }
-            setResult(prevResult => prevResult.concat(message));
-    };
+const levelNumbControll = () => {
+  const numbers = [];
+  let start, end;
+  if (level === 1) {
+    start= 1;
+    end=10;
+  }
+  if (level === 2) {
+    start= 1;
+    end=50;
+  }
+  if (level === 3) {
+    start= 1;
+    end=100;
+  }
+  for (let i= start; i <= end; i++){
+    numbers.push(
+      <button key={i} className="numberBtn" onClick={() => handleNumberClick(i)}>
+          {i}
+      </button>
+  );
+  }
+  return numbers;
+};
 
 
-return (
-  <div className="mysteryNumberGame">
-    <div className='title'>
-      <h1>Mystery Number</h1>
-    </div>
-    <div className='content'>
-      <div className='left'>
-        <div className='numbers'>
-          {Array.from({ length: 100 }, (_, i) => i + 1).map((number) => (
-            <button key={number} className="numberBtn" onClick={() => handleNumberClick(number)}>
-              {number}
-            </button>
-          ))}
-        </div>
-        <div className='levels'>
-          <button className='level'>Level 3</button>
-          <button className='level'>Level 2</button>
-          <button className='level'>Level 1</button>
-        </div>
 
+  const handleResult = (number) => {
+    let message = '';
+    if (chance <= 3) {
+      if (number < mysteryNumber) {
+        message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} ? ... +`;
+      } else if (number > mysteryNumber) {
+        message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} ? ... -`;
+      } else if (number === mysteryNumber) {
+        message = `${chance}${chance === 1 ? 'st' : chance === 2 ? 'nd' : chance === 3 ? 'rd' : 'th'} Try : ${number} Congratulations!You guessed the mystery number`;
+        setGameOver(true)
+      }
+
+      setChance(chance + 1);
+
+
+    } else {
+      message = `Sorry, you ran out of chances. The mystery number was ${mysteryNumber}.`;
+      setGameOver(true)
+    }
+    setResult(prevResult => prevResult.concat(message));
+  };
+
+
+  return (
+    <div className="mysteryNumberGame">
+      <div className='title'>
+        <h1>Mystery Number</h1>
       </div>
+      <div className='content'>
+        <div className='left'>
+          <div className='numbers'>
+            {levelNumbControll()}
+          </div>
+          <div className='levels'>
+            <button className= {level === 1 ? 'level active' : 'level'} onClick={() => handleLevelClick(1)}>Level 1</button>
+            <button className= {level === 2 ? 'level active' : 'level'} onClick={() => handleLevelClick(2)}>Level 2</button>
+            <button className= {level === 3 ? 'level active' : 'level'} onClick={() => handleLevelClick(3)}>Level 3</button>
+           
+          </div>
 
-      {/* <div className='restartSection'>
+        </div>
+
+        {/* <div className='restartSection'>
           <button>Restart</button>
         </div> */}
 
 
-      <div className='right'>
-        {result.map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}      </div>
+        <div className='right'>
+          {result.map((message, index) => (
+            <p key={index}>{message}</p>
+          ))}      </div>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default MysteryNumberMain;
